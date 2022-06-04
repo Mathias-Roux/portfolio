621f8ca01499f4e8063f615df316e00504057b47
Mathias Roux <mathiasroux.pro@gmail.com>
2022-06-04 15:34:38 +0200

    animation technos div done

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+import gsap from 'gsap'
+
 // Animation card
 const cards = document.querySelectorAll('.card')
 
@@ -12,4 +14,20 @@ const sideBar = document.querySelector('.sideBar')
 
 sideBar.addEventListener('click', event => {
 	document.body.classList.toggle('darkMode')
+})
+
+
+const technoLines = [...document.querySelectorAll('.paragraph p')]
+
+technoLines.forEach(line => {
+	gsap.set(line, {
+		y: '100%',
+		opacity: 0
+	})
+	gsap.to(line, {
+		y: '0%',
+		opacity: 1,
+		duration: 0.7,
+		ease: 'Power3.In'
+	})
 })
